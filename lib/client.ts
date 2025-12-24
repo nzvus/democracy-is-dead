bba637b68aea0869464126508a04ee172403e042
nzvus <dinoambrogio.onorati@mail.polimi.it>
2025-12-25 00:30:15 +0100

    0.3.3: voting&results

--- lib/client.ts
+++ lib/client.ts
@@ -1,22 +1,15 @@
 import { createBrowserClient } from '@supabase/ssr'
 
-// Variable to hold the single instance
 let client: ReturnType<typeof createBrowserClient> | undefined
 
 export function createClient() {
-  // 1. Check if we already have a client. If yes, return it.
   if (client) return client
 
-  // 2. If not, create a new one
   const url = process.env.NEXT_PUBLIC_SUPABASE_URL
   const key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
-  if (!url || !key) {
-    throw new Error("Missing Supabase environment variables")
-  }
+  if (!url || !key) throw new Error("Missing Supabase env vars")
 
   client = createBrowserClient(url, key)
-
-  // 3. Return the new client
   return client
 }
